2c4e63a0793b2780619ad41f429b0a8c7cd6d3b9
JinHyuck Cha <jhcha@superb-ai.com>
2022-06-06 20:12:42 +0900

    Add ksqldb client

---
+++ src/ksqldb.py
@@ -0,0 +1,7 @@
+from ksql import KSQLAPI
+
+client = KSQLAPI(
+    url="https://pksqlc-03kvq.ap-northeast-2.aws.confluent.cloud:443",
+    api_key="GNDPZPGQ263RLDUW",
+    secret="I3lV2PFVd8V/v7GKk1Yi8tfY/Aey/0YFmTDZ3hHgN/gpiAjaXbVtr/tHd1vSghbY",
+)
