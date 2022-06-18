62d794cd975e333d6f04f1683cd624aa5d13cc5d
Yonghwan <yhyoo@superb-ai.com>
2022-06-18 20:58:49 +0900

    multiprocessing (wip)

--- simulator/main.py
+++ simulator/main.py
@@ -2,24 +2,68 @@ import json
 import os
 import time
 from multiprocessing import Pool
+from typing import List
 
 import ijson
-from confluent_kafka import Producer
 
 from model import Image, Info, LabelFlattened
 from node import Node
 
 
-def simulate_node(conf):
-    node = Node(conf)
-    node.produce_label()
-    node.produce_info()
+def simulate_node(args):
+    print("simulating node")
+    node = Node(args["conf"])
+    node.produce_labels(args["labels"])
+    node.produce_info(args["info"])
+
+
+def parse_image(image: Image):
+    image_id = image.name.replace("jpg", "json")
+
+    # image's labels
+    labels = [
+        LabelFlattened(
+            id=f"{image.name}#{label.id}",
+            category=label.category,
+            scene=image.attributes.scene,
+            timeofday=image.attributes.timeofday,
+            weather=image.attributes.weather,
+        )
+        for label in image.labels
+    ]
+
+    # image's info
+    with open(os.path.join(info_dir, image_id)) as rf:
+        info_dict = json.loads(rf.read())
+        info = Info(**info_dict)
+
+    return labels, info
 
 
 if __name__ == "__main__":
     conf = {"bootstrap.servers": "localhost:29092"}
 
-    with Pool(2) as p:
-        while True:
-            p.map(simulate_node, [conf])
-            time.sleep(10)
+    info_dir = "info"
+
+    while True:
+        with open("labels/bdd100k_labels_images_val.json") as rf:
+            data = ijson.items(rf, "item")
+            jobs: List[dict] = []
+            for image_dict in data:
+                # batch 2
+                if len(jobs) == 2:
+                    with Pool(2) as p:
+                        p.map(simulate_node, jobs)
+                    jobs = []
+                    time.sleep(2)
+
+                image = Image(**image_dict)
+                labels, info = parse_image(image=image)
+                args = {"conf": conf, "labels": labels, "info": info}
+                jobs.append(args)
+
+            if len(jobs):
+                with Pool(2) as p:
+                    p.map(simulate_node, jobs)
+                jobs = []
+                time.sleep(2)
--- simulator/node.py
+++ simulator/node.py
@@ -1,54 +1,30 @@
 import json
-import os
-import time
+from typing import List
 
-import ijson
 from confluent_kafka import Producer
 
-from model import Image, Info, LabelFlattened
+from model import Info, LabelFlattened
 
 
 class Node:
     def __init__(self, conf) -> None:
         self.producer = Producer(conf)
 
-    def produce_label(self):
+    def produce_labels(self, labels: List[LabelFlattened]):
 
-        with open("labels/bdd100k_labels_images_val.json") as rf:
-            data = ijson.items(rf, "item")
-            for image_dict in data:
-                image = Image(**image_dict)
-                for label in image.labels:
-                    label_flattened = LabelFlattened(
-                        id=f"{image.name}#{label.id}",
-                        category=label.category,
-                        scene=image.attributes.scene,
-                        timeofday=image.attributes.timeofday,
-                        weather=image.attributes.weather,
-                    )
+        for label in labels:
+            self.producer.produce(
+                topic="labels",
+                value=json.dumps(label.dict()),
+                key=label.id,
+            )
+            self.producer.flush()
 
-                    self.producer.produce(
-                        topic="labels",
-                        value=json.dumps(label_flattened.dict()),
-                        key=label_flattened.id,
-                    )
-                    self.producer.flush()
+    def produce_info(self, info: Info):
 
-    def produce_info(self):
-
-        directory = "info"
-        for filename in os.listdir(directory):
-            f = os.path.join(directory, filename)
-            # checking if it is a file
-            if not os.path.isfile(f):
-                continue
-            with open(f) as rf:
-                info_dict = json.loads(rf.read())
-                info = Info(**info_dict)
-
-                self.producer.produce(
-                    topic="info",
-                    value=json.dumps(info.dict()),
-                    key=info.id,
-                )
-                self.producer.flush()
+        self.producer.produce(
+            topic="info",
+            value=json.dumps(info.dict()),
+            key=info.id,
+        )
+        self.producer.flush()
