8e448ae6f70245caf9e4bec7802023fe6e219842
JinHyuck Cha <jhcha@superb-ai.com>
2022-06-18 20:23:24 +0900

    deprecated weather info

--- src/streamlit/visualizer.py
+++ src/streamlit/visualizer.py
@@ -1,7 +1,43 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import numpy as np
 
+sample_datas = [
+    {
+        "header": {
+            "queryId": "query_1655548648091",
+            "schema": "`CATEGORY` STRING KEY, `TOTAL` BIGINT",
+        }
+    },
+    {"row": {"columns": ["a", 0]}},
+    {"row": {"columns": ["b", 0]}},
+    {"row": {"columns": ["bike", 67]}},
+    {"row": {"columns": ["bus", 93]}},
+    {"row": {"columns": ["c", 0]}},
+    {"row": {"columns": ["car", 85]}},
+    {"row": {"columns": ["d", 0]}},
+    {"row": {"columns": ["drivable area", 96]}},
+    {"row": {"columns": ["e", 0]}},
+    {"row": {"columns": ["lane", 72]}},
+    {"row": {"columns": ["motor", 77]}},
+    {"row": {"columns": ["person", 91]}},
+    {"row": {"columns": ["rider", 85]}},
+    {"row": {"columns": ["traffic light", 86]}},
+    {"row": {"columns": ["traffic sign", 88]}},
+    {"row": {"columns": ["train", 81]}},
+    {"row": {"columns": ["truck", 79]}},
+]
+
+
+df = pd.DataFrame()
+for data in sample_datas:
+    if data.get("row"):
+        row = data["row"]["columns"]
+        df = df.append(
+            pd.DataFrame([row], columns=["category", "total"]),
+            ignore_index=True,
+        )
 # set page layout
 st.set_page_config(
     page_title="Streamlit Map",
@@ -12,40 +48,23 @@ st.set_page_config(
 
 st.title("Streamlit Map")
 
-st.sidebar.subheader("weather")
-weather_sunny = st.sidebar.checkbox("sunny")
-weather_rainiy = st.sidebar.checkbox("rainy")
-weather_foggy = st.sidebar.checkbox("foggy")
+# st.sidebar.subheader("weather")
+# weather_sunny = st.sidebar.checkbox("sunny", value=True)
+# weather_rainiy = st.sidebar.checkbox("rainy", value=True)
+# weather_foggy = st.sidebar.checkbox("foggy", value=True)
 
-# data
-df = pd.DataFrame(
-    {
-        "latitude": [37.77, 37.77, 37.77, 37.77],
-        "longitude": [
-            -122.42,
-            -122.42,
-            -122.42,
-            -122.42,
-        ],
-        "weather": ["sunny", "rainy", "foggy", "sunny"],
-        "cateogry": ["car", "car", "turck", "human"],
-        "count": [1, 2, 3, 4],
-    }
-)
+# # Plot the GPS coordinates on the map
+# # st.map(df)
 
-# Plot the GPS coordinates on the map
-st.map(df)
+# st.subheader("label info")
+# if weather_sunny:
+#     condition = df["weather"] == "partly cloudy"
+# if weather_foggy:
+#     condition = df["weather"] == "foggy"
+# if weather_rainiy:
+#     condition = df["weather"] == "rainy"
+# df = df[condition]
 
-st.subheader("label info")
-if weather_sunny:
-    condition = df["weather"] == "sunny"
-    df = df[condition]
-if weather_foggy:
-    condition = df["weather"] == "foggy"
-    df = df[condition]
-if weather_rainiy:
-    condition = df["weather"] == "rainy"
-    df = df[condition]
 
-fig = px.pie(df, values="count", names="cateogry")
+fig = px.pie(df, values="total", names="category")
 st.write(fig)
