9b1e4e8dc8031a041032288c82758b15b50b8ca5
jinhyuck cha <jhcha@superb-ai.com>
2022-06-22 20:28:53 +0900

    Add map visualize

--- frontend/labels.py
+++ frontend/labels.py
@@ -2,6 +2,24 @@ import json
 from ksql import KSQLAPI
 
 
+def get_gps_index() -> list:
+    client = KSQLAPI("http://localhost:8088")
+    query = client.query("SELECT LONGITUDE, LATITUDE FROM gps_query")
+
+    results = []
+
+    while True:
+        try:
+            result = next(query)
+            results.append(result)
+        except RuntimeError:
+            break
+
+    concat_result = "".join(results)
+    result_dict = json.loads(concat_result)
+    return result_dict
+
+
 def get_category_index() -> list:
     client = KSQLAPI("http://localhost:8088")
     query = client.query("SELECT * FROM category_index")
